c892542d0f5f33bdbb38b2bf3fd9dff09cacc9d4
HAL001 <hal001bot@gmail.com>
2026-06-11 00:18:59 -0400

    Use trophy image for World Cup logo

--- server.js
+++ server.js
@@ -231,7 +231,7 @@ function competitionLabel(type) {
 
 function competitionLogo(type) {
   if (type === 'champions_league') return '/img/champions-league.png';
-  if (type === 'world_cup_2026') return '/img/world-cup-2026.svg';
+  if (type === 'world_cup_2026') return '/img/world-cup-2026.png';
   return '/img/logo.png';
 }
 
